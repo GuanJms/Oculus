c46f7d37f5b6c951f53ca5c060f142220cd6659c
GuanJms <45347032+GuanJms@users.noreply.github.com>
2024-03-14 22:05:05 -0400

    update
    update

---
+++ execution_module/execution_module_section/execution_signal_module/signal_template/single_dte_signal.py
@@ -0,0 +1,8 @@
+from execution_module.execution_module_section.execution_signal_module.execution_signal import ExecutionSignal
+
+
+class SingleDTESignal(ExecutionSignal):
+
+    def __init__(self):
+        self
+
--- strategy_module/decorator_module/selection_module/universal_decorator/expiration_DTE_rule.py
+++ strategy_module/decorator_module/selection_module/universal_decorator/expiration_DTE_rule.py
@@ -2,6 +2,8 @@ from typing import Optional, Tuple, List
 
 from execution_module.execution_module_section.execution_action_module.exectuion_action import ExecutionAction
 from execution_module.execution_module_section.execution_signal_module.execution_signal import ExecutionSignal
+from execution_module.execution_module_section.execution_signal_module.signal_template.single_dte_signal import \
+    SingleDTESignal
 from strategy_module.decorator_module.rule_decorator import RuleDecorator
 
 
@@ -36,10 +38,30 @@ class ExpirationDTERule(RuleDecorator):
     def execute(self) -> Tuple[List[ExecutionSignal], List[ExecutionAction]]:
         execution_signal_list, execution_action_list = self.rule.execute()
 
+    def signal_generator(self) -> List[ExecutionSignal]:
+        type = self.get_dte_rule_type()
+        if type == 'single':
+            signal_list = self._generate_single_dte_signal()
+            return signal_list
+        if type == 'range':
+            signal_list = self._generate_range_dte_signal()
+            return signal_list
 
-
+    def get_dte_rule_type(self) -> str:
+        if self._dte is not None and self._dte_min is None and self._dte_max is None:
+            return 'single'
+        if self._dte_min is not None or self._dte_max is not None:
+            return 'range'
 
     def _update_rule_param(self):
         expiration_rule = {'rule_type': self.rule_type, 'rule_name': self.rule_name,
                            'method': self._method, 'DTE': self._dte, 'DTE_min': self._dte_min, 'DTE_max': self._dte_max}
         self._expiration_rule.update(expiration_rule)
+
+    def _generate_single_dte_signal(self) -> List[ExecutionSignal]:
+        single_dte_singal = SingleDTESignal(self._dte)
+        return [single_dte_singal]
+
+    def _generate_range_dte_signal(self) -> List[ExecutionSignal]:
+        range_dte_signal = RangeDTESignal(target_dte = self._dte, min_dte = self._dte_min, max_dte = self._dte_max)
+        return [range_dte_signal]
